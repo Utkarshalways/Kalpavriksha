db5564d174d2c07710327f6a4446abdb10162001
utkarshalways <utkarshshrma08@gmail.com>
2025-01-28 17:26:47 +0530

    Updated: Assignment-6 Managing BookStore Using LinkedList

--- ManagingBookStoreUsingLinkedList.c
+++ ManagingBookStoreUsingLinkedList.c
@@ -235,7 +235,7 @@ int main(){
                     break;
 
             // case 3:
-            case 5: printf("%d",getTotalBooks());
+            case 5: printf("%d\n",getTotalBooks());
                     break;
 
 
